b99865de1dd5c5c1a26b10397ad79c43a971a8b3
Myron Apostolakis <myapos@gmail.com>
2026-01-01 16:44:29 +0200

    linter (#10)
    * linter
    
    * Add shared pre-push hook in .githooks/

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        process: "readonly",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": "off",
+    },
+  },
+];
